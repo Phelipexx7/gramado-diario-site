497942e79b6652d0baf38fdf191a8a8aacc9145f
Phelipexx7 <comercialparacatu01@gmail.com>
2026-03-10 16:43:33 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
+import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Galeria from "./pages/Galeria";
+import Mapa from "./pages/Mapa";
+import Reservas from "./pages/Reservas";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/mapa" element={<Mapa />} />
+        <Route path="/reservas" element={<Reservas />} />
+      </Routes>
+
+      <a
+        className="whatsapp"
+        href="https://wa.me/559999999999"
+        target="_blank"
+      >
+        💬
+      </a>
+    </>
+  );
+}
