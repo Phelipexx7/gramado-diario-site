8ef5ad0df7ba3f8a4e854e914bc136a35daa4151
Phelipexx7 <comercialparacatu01@gmail.com>
2026-03-10 16:44:50 -0300

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom";
 
+export default function Navbar() {
+  return (
+    <nav className="nav">
+
+      <h2>Gramado Diário</h2>
+
+      <div>
+
+        <Link to="/">Home</Link>
+        <Link to="/galeria">Galeria</Link>
+        <Link to="/mapa">Mapa</Link>
+        <Link to="/reservas">Reservas</Link>
+
+      </div>
+
+    </nav>
+  );
+}
